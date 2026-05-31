7779d35a9f650bad6278f75400d8d8c72e5c7b8c
Andres Procel <a.procel9634@gmail.com>
2026-05-31 18:40:47 -0500

    About actualizar

--- frontend/README.md
+++
@@ -1,9 +0,0 @@
-# MercApp
-
-This template should help get you started developing with Vue 3 in Vite.
-
-## Datos del estudiante
-  - Andres Procel
-  - aprocelb@uest.ups.edu.ec
-## Funcionalidades implementadas 
-## Instrucciones básicas de uso
--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -6,6 +6,7 @@
 
   <p>
     Aplicación creada con Vue 3 y Express.
+    La aplicación MercApp es una tienda virtual sencilla donde los usuarios pueden visualizar un catálogo de productos, buscar artículos por nombre, filtrarlos por categorías y revisar información detallada de cada producto. Tiene un carrito de compras que permite agregar o eliminar productos, calcular automáticamente el total y mantener la información guardada incluso al cerrar el navegador. El sistema incluye formularios para crear y editar productos con validaciones básicas para evitar errores en los datos ingresados. La aplicación cuenta con navegación entre páginas sin recargar el sitio, manejo de errores, carga optimizada de vistas y una conexión completa con un API REST que administra productos y categorías de forma dinámica.
   </p>
 
 </template>
